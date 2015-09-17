97d250931ba0a7df732175e1fcb494a66aad84e2
dan bergman <dan.bergman@guest.us.schott.com>
2015-09-17 11:38:39 -0400

    adding build folder

---
+++ src/AureliaNavigationASPNET5/build/paths.js
@@ -0,0 +1,16 @@
+﻿var path = require('path');
+
+var appRoot = 'wwwroot/src/';
+var outputRoot = 'wwwroot/dist/';
+
+module.exports = {
+    root: appRoot,
+    source: appRoot + '*.js',
+    html: appRoot + '*.html',
+    css: appRoot + '*.css',
+    style: 'styles/*.css',
+    output: outputRoot,
+    doc: './doc',
+    e2eSpecsSrc: '../wwwroot/test/e2e/src/*.js',
+    e2eSpecsDist: '../wwwroot/test/e2e/dist/'
+};
---
+++ src/AureliaNavigationASPNET5/build/tasks/serve.js
@@ -0,0 +1,19 @@
+﻿var gulp = require('gulp');
+var browserSync = require('browser-sync');
+
+// this task utilizes the browsersync plugin
+// to create a dev server instance
+// at http://localhost:43040
+gulp.task('serve', function (done) {
+    browserSync({
+        open: false,
+        port: 9000,
+        server: {
+            baseDir: ['.'],
+            middleware: function (req, res, next) {
+                res.setHeader('Access-Control-Allow-Origin', '*');
+                next();
+            }
+        }
+    }, done);
+});
